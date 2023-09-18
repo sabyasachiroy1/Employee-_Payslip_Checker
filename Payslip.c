840fdbded432cf75a1a4b20a0837c37ab1275058
SABYASACHI ROY <110336884+sabyasachiroy1@users.noreply.github.com>
2023-09-18 12:29:38 +0530

    Payslip.c

---
+++ Payslip.c
@@ -0,0 +1,248 @@
+#include<stdio.h>
+int main ()
+{
+	//VARIABLE DECLEARTION
+    int a,b,e,f;
+	//EMPLOYESS LOGIN PAGE
+   
+    printf ("\n----------------------------------------------------");
+    printf ("\n\n\n\t\tABC PRIVATE LIMITED \n\t\tPAYROLL MANAGMENT SYSTEM");
+    printf ("\n\n\t\tEmployee Id: ");
+    scanf ("%d",&a);
+    printf("\n\t\tPassword: ");
+    scanf ("%d",&b);
+    //employee1
+    if (a==91100 && b==1289)
+    {
+    printf ("\n----------------------------------------------------");
+    printf ("\n\n\n\t\tABC PRIVATE LIMITED");
+    printf ("\n\nEmployee Name: M.Das\t\tEmployee Id:%dABC ",a);
+    printf ("\nDesignation: It Assis.\t\t\tEmployee PF no: IPABC%dF ",a);
+    printf ("\nBank Account No:657565756\t\tIFCS:SBI%d7Y",a);
+    printf ("\nPAN:MR%dDAS\t\t\t\tLocation:India ",a);
+    printf ("\n----------------------------------------------------------\n");
+    printf("\nBasic Pay:    12000.00\n");
+    printf("Hra\t :     2000.00\n");
+    printf("Da\t :     2000.00");
+    printf("\nSpecialAlow:  12000.00\n");
+    printf("\n_____________________________________________________\n");
+    printf("Total income: 28000.00\t\t\tDeduction\n\t\t\t\t\tPF:1800.00\n\t\t\t\t\tTax:200.00");
+    printf("\n_____________________________________________________\n");
+    printf("\nNet Pay:26000.00 \n");
+    printf("_____________________________________________________");
+    printf ("\n\n\t\tFor help contact\n");
+    printf ("\n\n\n\t\tABC PRIVATE LIMITED");
+    printf ("\n\t\tMail: abc.org@mail.com\n\t\tMobile: 090909909\n\t\tWebsite: www.abc.org.com\n\t\tAdress: 12,Edr road,India,Delhi-90;\n\t\t[Office time Mondy -Friday(9.30 Am-7.00Pm)]\n\n");
+    }
+    //employee2
+    else if (a==96101 && b==10089)
+    {
+    printf ("\n----------------------------------------------------");
+    printf ("\n\n\n\t\tABC PRIVATE LIMITED");
+    printf ("\n\nEmployee Name: C.Roy\t\tEmployee Id:%dABC ",a);
+    printf ("\nDesignation: It Devl.\t\t\tEmployee PF no: IPABC%dF ",a);
+    printf ("\nBank Account No:90909856\t\tIFCS:PNB%d7Y",a);
+    printf ("\nPAN:MR%dROY\t\t\t\tLocation:India ",a);
+    printf ("\n----------------------------------------------------------\n");
+    printf("\nBasic Pay:    12000.00\n");
+    printf("Hra\t :     2000.00\n");
+    printf("Da\t :     2000.00");
+    printf("\nSpecialAlow:  14000.00\n");
+    printf("\n_____________________________________________________\n");
+    printf("Total income: 30000.00\t\t\tDeduction\n\t\t\t\t\tPF:1800.00\n\t\t\t\t\tTax:200.00");
+    printf("\n_____________________________________________________\n");
+    printf("\nNet Pay:28000.00 \n");
+    printf("_____________________________________________________");
+    printf ("\n\n\t\tFor help contact\n");
+    printf ("\n\n\n\t\tABC PRIVATE LIMITED");
+    printf ("\n\t\tMail: abc.org@mail.com\n\t\tMobile: 090909909\n\t\tWebsite: www.abc.org.com\n\t\tAdress: 12,Edr road,India,Delhi-90;\n\t\t[Office time Mondy -Friday(9.30 Am-7.00Pm)]\n\n");
+
+    }
+    //employee3
+ else if (a==93103 && b==1008)
+    {
+    printf ("\n----------------------------------------------------");
+    printf ("\n\n\n\t\tABC PRIVATE LIMITED");
+    printf ("\n\nEmployee Name: R.Bose\t\tEmployee Id:%dABC ",a);
+    printf ("\nDesignation: Programmer.\t\t\tEmployee PF no: IPABC%dF ",a);
+    printf ("\nBank Account No:563273256\t\tIFCS:SNB%d7Y",a);
+    printf ("\nPAN:MR%dBOSE\t\t\t\tLocation:India ",a);
+    printf ("\n----------------------------------------------------------\n");
+    printf("\nBasic Pay:    12000.00\n");
+    printf("Hra\t :     2000.00\n");
+    printf("Da\t :     2000.00");
+    printf("\nSpecialAlow:  16000.00\n");
+    printf("\n_____________________________________________________\n");
+    printf("Total income: 32000.00\t\t\tDeduction\n\t\t\t\t\tPF:1800.00\n\t\t\t\t\tTax:200.00");
+    printf("\n_____________________________________________________\n");
+    printf("\nNet Pay:30000.00 \n");
+    printf("_____________________________________________________");
+    printf ("\n\n\t\tFor help contact\n");
+    printf ("\n\n\n\t\tABC PRIVATE LIMITED");
+    printf ("\n\t\tMail: abc.org@mail.com\n\t\tMobile: 090909909\n\t\tWebsite: www.abc.org.com\n\t\tAdress: 12,Edr road,India,Delhi-90;\n\t\t[Office time Mondy -Friday(9.30 Am-7.00Pm)]\n\n");
+    }
+    //employee4
+     else if (a==91104 && b==21089)
+    {
+    printf ("\n----------------------------------------------------");
+    printf ("\n\n\n\t\tABC PRIVATE LIMITED");
+    printf ("\n\nEmployee Name: S.Das\t\tEmployee Id:%dABC ",a);
+    printf ("\nDesignation: Support.\t\t\tEmployee PF no: IPABC%dF ",a);
+    printf ("\nBank Account No:0093273256\t\tIFCS:RNB%d7Y",a);
+    printf ("\nPAN:MR%ddas\t\t\t\tLocation:India ",a);
+    printf ("\n----------------------------------------------------------\n");
+    printf("\nBasic Pay:    12000.00\n");
+    printf("Hra\t :     8000.00\n");
+    printf("Da\t :     2000.00");
+    printf("\nSpecialAlow:  16000.00\n");
+    printf("\n_____________________________________________________\n");
+    printf("Total income: 38000.00\t\t\tDeduction\n\t\t\t\t\tPF:1800.00\n\t\t\t\t\tTax:200.00");
+    printf("\n_____________________________________________________\n");
+    printf("\nNet Pay:36000.00 \n");
+    printf("_____________________________________________________");
+    printf ("\n\n\t\tFor help contact\n");
+    printf ("\n\n\n\t\tABC PRIVATE LIMITED");
+    printf ("\n\t\tMail: abc.org@mail.com\n\t\tMobile: 090909909\n\t\tWebsite: www.abc.org.com\n\t\tAdress: 12,Edr road,India,Delhi-90;\n\t\t[Office time Mondy -Friday(9.30 Am-7.00Pm)]\n\n");
+}
+
+//employee5
+
+    else if (a==81104 && b==980089)
+    {
+    printf ("\n----------------------------------------------------");
+    printf ("\n\n\n\t\tABC PRIVATE LIMITED");
+    printf ("\n\nEmployee Name: B.Kundu\t\tEmployee Id:%dABC ",a);
+    printf ("\nDesignation: Counsultant.\t\t\tEmployee PF no: IPABC%dF ",a);
+    printf ("\nBank Account No:0091233256\t\tIFCS:RNB%d7Y",a);
+    printf ("\nPAN:MR%dKUNDU\t\t\t\tLocation:India ",a);
+    printf ("\n----------------------------------------------------------\n");
+    printf("\nBasic Pay:    12000.00\n");
+    printf("Hra\t :     8000.00\n");
+    printf("Da\t :     9000.00");
+    printf("\nSpecialAlow:  18000.00\n");
+    printf("\n_____________________________________________________\n");
+    printf("Total income: 47000.00\t\t\tDeduction\n\t\t\t\t\tPF:1800.00\n\t\t\t\t\tTax:200.00");
+    printf("\n_____________________________________________________\n");
+    printf("\nNet Pay:45000.00 \n");
+    printf("_____________________________________________________");
+    printf ("\n\n\t\tFor help contact\n");
+    printf ("\n\n\n\t\tABC PRIVATE LIMITED");
+    printf ("\n\t\tMail: abc.org@mail.com\n\t\tMobile: 090909909\n\t\tWebsite: www.abc.org.com\n\t\tAdress: 12,Edr road,India,Delhi-90;\n\t\t[Office time Mondy -Friday(9.30 Am-7.00Pm)]\n\n");
+}
+
+//employee6
+
+    else if (a==96108 && b==94565)
+    {
+        printf ("\n----------------------------------------------------");
+    printf ("\n\n\n\t\tABC PRIVATE LIMITED");
+    printf ("\n\nEmployee Name: B.Banerjee\t\tEmployee Id:%dABC ",a);
+    printf ("\nDesignation: HR.\t\tEmployee PF no: IPABC%dF ",a);
+    printf ("\nBank Account No:000564784\t\tIFCS:RNB%d7Y",a);
+    printf ("\nPAN:MR%dBANERJEE\t\t\t\tLocation:India ",a);
+    printf ("\n----------------------------------------------------------\n");
+    printf("\nBasic Pay:    12000.00\n");
+    printf("Hra\t :     8000.00\n");
+    printf("Da\t :     10000.00");
+    printf("\nSpecialAlow:   0.00\n");
+    printf("\n_____________________________________________________\n");
+    printf("Total income: 30000.00\t\t\tDeduction\n\t\t\t\t\tPF:1800.00\n\t\t\t\t\tTax:200.00");
+    printf("\n_____________________________________________________\n");
+    printf("\nNet Pay:28000.00 \n");
+    printf("_____________________________________________________");
+    printf ("\n\n\t\tFor help contact\n");
+    printf ("\n\n\n\t\tABC PRIVATE LIMITED");
+    printf ("\n\t\tMail: abc.org@mail.com\n\t\tMobile: 090909909\n\t\tWebsite: www.abc.org.com\n\t\tAdress: 12,Edr road,India,Delhi-90;\n\t\t[Office time Mondy -Friday(9.30 Am-7.00Pm)]\n\n");
+}
+
+
+    //Admin LOGIN
+    else if (a==1029 &&b==5648332)
+		//ADMIN DASHBOARD
+    {
+        printf ("\n----------------------------------------------------");
+        printf ("\n\n\n\t\tABC PRIVATE LIMITED");
+        printf ("\n\t\tAdmin Pannel");
+        printf ("\n\n\t\tEmployee Details--0\n\t\tEmployes Salary status--1");
+        printf ("\n\n\t\tEnter Choice: ");
+        scanf ("%d",&e);
+        if (e==0)
+        {
+            printf ("\n\n\t\tEmployee Id: ");
+            scanf ("%d",&f);
+            printf ("\t\t------------------------");
+            
+			//EMPLOYESS DETAILS
+            if(f==91100)
+            {
+                
+                
+               printf ("\n\t\t--------EMPLOYEE-ID: %dABC---------",f);
+                printf ("\n\n\nEmployee Name: Mr.Das\t\t\t Employee Id:%dABC ",a);
+                printf ("\nDesignation: It Assis.\t\t\t Employee PF no: IPABC%dF ",a);
+                printf ("\nBank Account No:657565756\t\t IFCS:SBI%d7Y",a);
+                printf ("\nPAN:MR%dDAS\t\t\t\t  Location:India\n\n ",a);
+
+            }
+            else if(f==96101)
+            {
+                  printf ("\n\n\n--------EMPLOYEE-ID: %dABC---------",f);
+                printf ("\n\nEmployee Name: C.Roy\t\tEmployee Id:%dABC ",a);
+                printf ("\nDesignation: It Devl.\t\t\tEmployee PF no: IPABC%dF ",a);
+                printf ("\nBank Account No:90909856\t\tIFCS:PNB%d7Y",a);
+                printf ("\nPAN:MR%dROY\t\t\t\tLocation:India ",a);
+
+            }
+            else if(f==93103)
+            {
+                  printf ("\n\n\n--------EMPLOYEE-ID: %dABC---------",f);
+                printf ("\n\nEmployee Name: R.Bose\t\tEmployee Id:%dABC ",a);
+                printf ("\nDesignation: Programmer.\t\t\tEmployee PF no: IPABC%dF ",a);
+                printf ("\nBank Account No:563273256\t\tIFCS:SNB%d7Y",a);
+                printf ("\nPAN:MR%dBOSE\t\t\t\tLocation:India ",a);
+            }
+            else if (f==91104)
+            {
+                  printf ("\n\n\n--------EMPLOYEE-ID: %dABC---------",f);
+                printf ("\n\nEmployee Name: B.Kundu\t\tEmployee Id:%dABC ",a);
+                printf ("\nDesignation: Counsultant.\t\t\tEmployee PF no: IPABC%dF ",a);
+                printf ("\nBank Account No:0091233256\t\tIFCS:RNB%d7Y",a);
+                printf ("\nPAN:MR%dKUNDU\t\t\t\tLocation:India ",a);
+
+            }
+            else if (f==96108)
+            {
+                 printf ("\n\n\n--------EMPLOYEE-ID: %dABC---------",f);
+                printf ("\n\nEmployee Name: B.Banerjee\t\tEmployee Id:%dABC ",a);
+                printf ("\nDesignation: HR.\t\t\tEmployee PF no: IPABC%dF ",a);
+                printf ("\nBank Account No:000564784\t\tIFCS:RNB%d7Y",a);
+                printf ("\nPAN:MR%dBANERJEE\t\t\t\tLocation:India ",a);
+            }
+            
+        }
+		//SALARY CREDIT STATUS
+        else if (e==1)
+        {
+            printf("\n\n\t\t--EMPLOYEES SALARY STATUS---\n");
+            
+            printf("\n\n\nNo\t Id\t Name\tDesignation   Amount\t Status\t   Date\n");
+            printf("\n01.  |9110ABC|   |M.Das| |It.Assis|  |26000.00|   |Paid|  |01/10/2022|");
+            printf("\n02.  |96101ABC|  |C.Roy| |It.Devlo|  |28000.00|   |Paid|  |01/10/2022|");
+            printf("\n03.  |93103ABC|  |R.Bos| |Program |  |30000.00|   |Paid|  |01/10/2022|");
+            printf("\n04.  |9110ABC |  |S.Das| |SUPPORT |  |36000.00|   |Paid|  |01/10/2022|");
+            printf("\n05.  |96105ABC|  |B.Kun| |Consult |  |45000.00|   |Paid|  |01/10/2022|");
+            printf("\n06.  |96108ABC|  |J.Bnj| |  HR    |  |28800.00|   |Paid|  |01/10/2022|\n\n");
+        }
+        
+    }
+	//EMPLOY ENTERED WRONG DATA
+    else
+    {
+        printf("\n\t\tEmployee Id: %d is not Found\n\n\n\n",a);
+    }
+    
+   
+    return 0;
+}
+
+
